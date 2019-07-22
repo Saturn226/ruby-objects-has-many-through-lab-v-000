1696f4f5a0a8dd1783f3b2a0fe6440e7b60f2c85
Ashley Muller <serenehopes@gmail.com>
2019-07-22 23:21:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,2 +1,24 @@
 class Doctor
+  def initialize(name)
+    @name = name
+    @patients = []
+    @appointments = []
+  end
+  
+  def name
+    @name
+  end
+  
+  def add_appointment(patient, date)
+    @appointments << Appointment.new(patient, date)
+    
+  end
+  def appointments
+    @appointments
+  end
+  def patients
+   appointments.collect do |appointment|
+     appointment.patient
+   end
+  end
 end
