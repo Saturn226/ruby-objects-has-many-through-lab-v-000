0b5d310ba922e357527070341e31d4634e6667ce
Ashley Muller <serenehopes@gmail.com>
2019-07-22 23:23:15 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,14 @@
+class Appointmnent
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+  end
+  
+  def patient
+    self.patient
+  end
+  
+  def doctor
+    self.doctor
+  end
+end
