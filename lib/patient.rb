42d135cf80a06dad4cff94223673a26518360f2e
Ashley Muller <serenehopes@gmail.com>
2019-07-22 23:31:44 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,8 +1,10 @@
 class Patient
   def initialize(name)
     @name = name
+    @appointments = []
   end
   def add_appointment(appointment)
+    @appointment << appointment
   end
   
   def doctors
